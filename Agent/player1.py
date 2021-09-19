c391c72966f0ab1b700378ef6b291caffed4373a
Erfan Zekri Esfahani <40582518+eze1376@users.noreply.github.com>
2021-09-19 12:14:19 +0430

    initialize variables

--- Agent/player1.py
+++ Agent/player1.py
@@ -1,4 +1,39 @@
-from MessageController import MsgController
+# from MessageController import MsgController
+def Initialization(initMsg):
+    msgElements = initMsg.split(' ')
+    global mapHeight, mapWidth, playerX, playerY, playerHealth, playerBombRange, playerTrapCount, \
+         vision, bombDelay, maxBombRange, deadzoneStartingStep, deadzoneExpansionDelay, maxStep
+
+    # first initialization
+    mapHeight = msgElements[1]
+    mapWidth = msgElements[2]
+    playerX = msgElements[3]
+    playerY = msgElements[4]
+    playerHealth = msgElements[5]
+    playerBombRange = msgElements[6]
+    playerTrapCount = msgElements[7]
+    vision = msgElements[8]
+    bombDelay = msgElements[9]
+    maxBombRange = msgElements[10]
+    deadzoneStartingStep = msgElements[11]
+    deadzoneExpansionDelay = msgElements[12]
+    maxStep = msgElements[13]
+
+    print("init confirm")
+
+def MsgController(msg):
+    msgElements = msg.split(' ')
+    # print(msgElements)
+    if msgElements[0]=="init":
+        return("init confirm")
+    else:
+        return 0
+
+
+
+initMsg = input()
+Initialization(initMsg)
+
 
 gamePlaying = True
 
